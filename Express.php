48c1506ff6b80d9a0aabb7b6975b8d9d5c77026c
ztigma <48769371+ztigma@users.noreply.github.com>
2025-03-17 03:43:29 -0400

    View Class Added
    a View Class for make your best and easy views.

--- Express.php
+++ Express.php
@@ -9,6 +9,30 @@ class FileSystem
   {
       $this->conn = $conn;
   }
+    function guardarArchivosSubidos($archivos, $rutaDestino) {
+        if (!is_array($archivos) || empty($archivos) || !is_string($rutaDestino) || empty($rutaDestino)) {
+            return 'parametro fallido'; // Validación de parámetros
+        }
+    
+        if (!is_dir($rutaDestino)) {
+            if (!mkdir($rutaDestino, 0777, true)) {
+                return 'error al crear la carpeta'; // Error al crear la carpeta
+            }
+        }
+    
+        foreach ($archivos['tmp_name'] as $key => $tmpName) 
+        {
+            $nombreArchivo = basename($archivos['name'][$key]);
+            $rutaCompleta = $rutaDestino . '/' . $nombreArchivo;
+
+            if (!move_uploaded_file($tmpName, $rutaCompleta)) 
+            {
+                return 'error al mover el archivo'; // Error al mover el archivo
+            }
+        }
+    
+        return 'subida exitosa'; // Subida exitosa
+    }
   function controlador($file_name)
   {
     $controllerName = basename($file_name, ".php"); // '/' -> 'homeController.php' -> 'homeController'
@@ -296,4 +320,205 @@ class App
     }
   }
 }
+class View {
+    public $tag = "div";
+    public $attributes = [];
+    public $children = [];
+    public $style = [];
+    private static $contador = 0;
+
+    public function __construct() {
+        $this->attributes = [
+            "id" => $this->getClassName() . "_" . $this->ID(),
+            "_view" => $this->getClassName(),
+            "_orientation" => "vertical_center",
+            "onclick" => "/*onclick*/",
+            "onchange" => "/*onchange*/",
+            "_onloading" => 'true'
+        ];
+        $this->style = [
+            "--h" => "auto",
+            "--w" => "auto",
+            "--h_phone" => "auto",
+            "--w_phone" => "auto",
+            '--box_shadow' => '2vw',
+            '--box_shadow_color' => 'black',
+            '--border_radius' => '0px',
+            '--border_radius_phone' => '0px',
+            '--font_size' => '1vw',
+            '--padding' => '0px',
+            '--padding_phone' => '0px',
+            '--margin' => 'auto',
+            '--margin_phone' => 'auto'
+        ];
+    }
+
+    private function getClassName() {
+        return "View";
+    }
+
+    private function ID() 
+    {
+        return ++self::$contador;
+    }
+
+    public function SetImg($src) 
+    {
+        $this->Style(['background' => "url('$src') center center no-repeat"]);
+        return $this;
+    }
+
+    public function SetImgContain($src) 
+    {
+        $this->Style(['background' => "url('$src') center center / contain no-repeat"]);
+        return $this;
+    }
+
+    public function SetBackImgContain($src) 
+    {
+        $this->Style([
+            'background-image' => "url('$src')",
+            'background-size' => 'contain',
+            'background-repeat' => 'no-repeat',
+            'background-position' => 'center'
+        ]);
+        return $this;
+    }
+
+    public function SetImgDefault($src) 
+    {
+        $this->Style(['background' => "url('$src')"]);
+        $this->Style(['background-position' => '0% 83%']);
+        return $this;
+    }
+
+    public function Tag($n) {
+        $this->tag = $n;
+        return $this;
+    }
+
+    public function Children($n) {
+        $this->children = $n;
+        return $this;
+    }
+
+    public function Children_add($n) {
+        foreach ($n as $c) {
+            $this->children[] = $c;
+        }
+        return $this;
+    }
+
+    public function Attributes($n) {
+        $this->attributes = array_merge($this->attributes, $n);
+        return $this;
+    }
+
+    public function Style($n) {
+        $this->style = array_merge($this->style, $n);
+        return $this;
+    }
+
+    public function AttributesToHtml() {
+        $attributesHtml = "";
+        foreach ($this->attributes as $key => $value) {
+            $attributesHtml .= "$key=\"$value\" ";
+        }
+        return trim($attributesHtml);
+    }
+
+    public function StyleToHtml() {
+        $styleHtml = "";
+        foreach ($this->style as $key => $value) {
+            $styleHtml .= "$key:$value;";
+        }
+        return $styleHtml ? "style=\"$styleHtml\"" : "";
+    }
+
+    public function __toString() {
+        $attributesHtml = $this->AttributesToHtml();
+        $styleHtml = $this->StyleToHtml();
+        $combinedAttributes = $attributesHtml . ($styleHtml ? ' ' . $styleHtml : '');
+
+        if ($this->tag == 'input' || $this->tag == "img") {
+            return "<{$this->tag} {$combinedAttributes}>";
+        } else {
+            $childrenHtml = is_array($this->children) ? implode('', array_map(function ($child) {
+                return (string) $child;
+            }, $this->children)) : (string) $this->children;
+            return "<{$this->tag} {$combinedAttributes}>{$childrenHtml}</{$this->tag}>";
+        }
+    }
+}
+
+trait StringExtensions {
+    public function HTML_TO_TTR() {
+        preg_match_all('/([^"]*)="([^"]*)"/', $this, $matches);
+        if (empty($matches[0])) {
+            return [];
+        }
+        $list = $matches[0];
+        $list[0] = explode(' ', $list[0])[1];
+        $ttr = [];
+        foreach ($list as $n) {
+            $split = explode('=', $n);
+            $ttr[$split[0]] = substr($split[1], 1, -1);
+        }
+        return $ttr;
+    }
+
+    public function HTML_TO_CHILDREN() {
+        preg_match('/>([\s\S]*)</', $this, $matches);
+        return isset($matches[1]) ? $matches[1] : '';
+    }
+
+    public function toView() {
+        $view = new View();
+        $view->attributes = $this->HTML_TO_TTR();
+        $view->children = $this->HTML_TO_CHILDREN();
+        $view->attributes['id'] = $view->attributes['_view'] . "_" . ++View::$contador;
+        return $view;
+    }
+
+    public function PROPS($props)
+    {
+        $result = $this;
+        foreach ($props as $key => $value) 
+        {
+            // Reemplazar tanto __name__ como /*name*/
+            $result = preg_replace("/(__" . $key . "__|\/\*" . $key . "\*\/)/", $value, $result);
+        }
+        return $result;
+    }
+}
+
+trait ObjectExtensions {
+    public function TO_INPUTS() {
+        $inputPrefab = "<input id=\"__name__\" name=\"__name__\" value=\"__value__\" style=\"display: none;\">";
+        $inputs = "";
+        foreach ((array) $this as $key => $value) {
+            $inputs .= $inputPrefab->PROPS(['name' => $key, 'value' => $value]);
+        }
+        return $inputs;
+    }
+}
+
+// Agregar los traits a las clases String y Object.
+String::class;
+Object::class;
+trait_exists('StringExtensions') && trait_exists('ObjectExtensions') && class_alias('StringExtensions','String');
+trait_exists('StringExtensions') && trait_exists('ObjectExtensions') && class_alias('ObjectExtensions','Object');
+
+// Ejemplo de uso:
+//$view = new View();
+//$view->Tag('p')->Children('¡Hola, mundo!')->Style(['color' => 'blue']);
+//echo $view;
+
+//$htmlString = '<div id="test" class="_view" data-attr="value">Child content</div>';
+//$viewFromString = $htmlString->toView();
+//echo $viewFromString;
+
+//$dataObject = (object) ['name' => 'John', 'age' => 30];
+//echo $dataObject->TO_INPUTS();
+
 ?>
